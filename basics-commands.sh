befbdd2ba862a395561c4e72c8a2528eec193fd1
joeldevel <christianjoel.developer@gmail.com>
2023-07-15 22:24:55 -0300

    links examples

--- basics-commands.sh
+++ basics-commands.sh
@@ -18,3 +18,19 @@ ls
 print_colored "Show contents of file with 'cat'";
 cat file1.txt
 
+
+print_colored "Count words in file with 'wc'(lines, words, characters)";
+wc file1.txt
+
+
+print_colored "Display working directory with 'pwd'";
+pwd
+
+print_colored "Link files with 'ln'; ln from to";
+ln file1.txt file1-link.txt
+ls -ls
+
+print_colored "Create a symbolic link with 'ln -s'";
+ln -s file1.txt file1-slink.txt
+ls -ls
+
